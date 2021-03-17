09fbc2f1e2f797a4ab178bd54cb4ff03e235c366
Sebastian Krossa <sebastian.krossa@ntnu.no>
2021-03-17 13:35:01 +0100

    Updated functionality - generate accumulated binary images of annotations with same name and class

--- export_for_st_toolbox.groovy
+++ export_for_st_toolbox.groovy
@@ -3,7 +3,7 @@
  * test with QuPath version 0.2.3
  * @author Sebastian Krossa
  * NTNU Trondheim, Norway
- * Jan/Feb 2021
+ * Jan/Feb/March 2021
  * sebastian.krossa@ntnu.no
  * based on & reuses code from Pete Bankhead / QuPath
  * https://gist.github.com/petebankhead/0b14beef131312042686c01056104b85
@@ -20,6 +20,7 @@ import qupath.lib.regions.RegionRequest
 import javax.imageio.ImageIO
 import java.awt.Color
 import java.awt.image.BufferedImage
+import static qupath.lib.gui.scripting.QPEx.*
 
 // Configure the following vars to your needs
 // image downsample factor -> the full image gets exported!
@@ -40,29 +41,30 @@ def gson = new GsonBuilder()
 
 
 //class for poly export
-class polyexp {
+class polyExp {
     Double centroidx
     Double centroidy
     Point2[] polygon
 }
 
 //class for cell export
-class cellexp {
+class cellExp {
     String name
-    polyexp cell_poly
-    polyexp nucleus_poly
+    polyExp cell_poly
+    polyExp nucleus_poly
     String type
 }
 
 //class for annotation export
-class annotexp {
+class annotExp {
     String name
-    polyexp shape
+    String className
+    polyExp shape
     String type
 }
 
 //class for export of whole slide data to JSON
-class img_cells_export {
+class imgCellsExport {
     String name
     String imgFileBaseName
     String imgType
@@ -70,8 +72,8 @@ class img_cells_export {
     Double pixelHeightMicron
     Double pixelWidthMicron
     Double avgPixelSizeMicron
-    def cells = new ArrayList<cellexp>()
-    def annotations = new ArrayList<annotexp>()
+    def cells = new ArrayList<cellExp>()
+    def annotations = new ArrayList<annotExp>()
 }
 
 
@@ -84,7 +86,7 @@ def server = imageData.getServer()
 def cells = hierarchy.getCellObjects()
 
 // get annotations
-def annotations = hierarchy.getFlattenedObjectList(null).findAll { it.isAnnotation() }
+def annotations = hierarchy.getAnnotationObjects()
 
 // define region - full image in this case
 def region = RegionRequest.createInstance(server.getPath(), downsample, 0, 0, server.getWidth(), server.getHeight())
@@ -106,7 +108,7 @@ String baseFileName = String.format('%s_(%.2f,%d,%d,%d,%d)_',
 
 def cal = server.getMetadata()
 //holds export data
-def export_to_json = new img_cells_export(
+def export_to_json = new imgCellsExport(
         name: cal.name,
         imgFileBaseName: baseFileName,
         imgType: imageExportType.toLowerCase(),
@@ -133,8 +135,11 @@ ImageIO.write(imgMask, 'PNG', fileMask)
 
 // now the annotations
 
-annotations.each {
-    generateAndSaveMasksAndAddJsonAnnotations(region, img, pathOutput, baseFileName, it, downsample, export_to_json)
+def sortedAnnotationsMap = sortAnnotations(annotations)
+
+sortedAnnotationsMap.each {
+    processAnnotationSet(it.key as String, it.value as Collection<PathObject>, region, img, pathOutput, baseFileName,
+            downsample, export_to_json)
 }
 
 
@@ -146,6 +151,8 @@ fileJson.text = gson.toJson(export_to_json)
 
 print 'Done!'
 
+// functions
+
 /**
  * Add extracted nucleus region to binary imgMask and add cell info, shape & coordinates to export_to_json object.
  *
@@ -156,7 +163,7 @@ print 'Done!'
  * @param export_to_json object that collects/hold detailed info on all cells for export to JSON
  * @return
  */
-def generateImageMaskAndJson(RegionRequest region, BufferedImage imgMask, PathObject cellObject, double downsample, img_cells_export export_to_json) {
+def generateImageMaskAndJson(RegionRequest region, BufferedImage imgMask, PathObject cellObject, double downsample, imgCellsExport export_to_json) {
     // Extract ROI of nucleus - change nucleus to getROI() to use whole cell instead for binary mask
     def roi = cellObject.nucleus
 
@@ -164,14 +171,14 @@ def generateImageMaskAndJson(RegionRequest region, BufferedImage imgMask, PathOb
         print 'Warning! No ROI for object ' + cellObject + ' - cannot export corresponding region & mask'
         return
     }
-    def tmp = new cellexp(
+    def tmp = new cellExp(
             name: cellObject.getDisplayedName().toString(),
-            cell_poly: new polyexp(
+            cell_poly: new polyExp(
                     centroidx: cellObject.getROI().getCentroidX(),
                     centroidy: cellObject.getROI().getCentroidY(),
                     polygon:  cellObject.getROI().getAllPoints()
             ),
-            nucleus_poly: new polyexp(
+            nucleus_poly: new polyExp(
                     centroidx: cellObject.nucleus.getCentroidX(),
                     centroidy: cellObject.nucleus.getCentroidY(),
                     polygon:  cellObject.nucleus.getAllPoints()
@@ -193,29 +200,30 @@ def generateImageMaskAndJson(RegionRequest region, BufferedImage imgMask, PathOb
 }
 
 /**
- * Generate and directly save binary masks for all annotations - attach annotation object to json export class
+ * Generate binary masks for annotation - attach annotation object to json export class
  *
  * @param region RegionRequest object corresponding to the imgMask
- * @param img the base image
- * @param pathOutput where the masks gets stored
- * @param baseFileName the common part of the filename
+ * @param imgMask the mask for this annotation group
  * @param annotationObject current annotation
  * @param downsample - downsample factor
  * @param export_to_json - the json export object
  * @return
  */
-
-def generateAndSaveMasksAndAddJsonAnnotations(RegionRequest region, BufferedImage img, String pathOutput, String baseFileName, PathObject annotationObject, double downsample, img_cells_export export_to_json) {
+def generateMaskAndAddJsonAnnotation(RegionRequest region, BufferedImage imgMask, PathObject annotationObject,
+                                     double downsample, imgCellsExport export_to_json) {
     // Extract ROI & classification name
     def roi = annotationObject.getROI()
     if (roi == null) {
         print 'Warning! No ROI for object ' + pathObject + ' - cannot export corresponding region & mask'
         return
     }
+    def className = annotationObject.getPathClass() == null ? 'None' : annotationObject.getPathClass().toString()
+    def annoName = annotationObject.name == null ? 'Annotation' : annotationObject.name
 
-    def tmp = new annotexp(
-            name: annotationObject.getDisplayedName().toString(),
-            shape: new polyexp(
+    def tmp = new annotExp(
+            name: annoName,
+            className: className,
+            shape: new polyExp(
                     centroidx: annotationObject.getROI().getCentroidX(),
                     centroidy: annotationObject.getROI().getCentroidY(),
                     polygon: annotationObject.getROI().getAllPoints()
@@ -224,12 +232,9 @@ def generateAndSaveMasksAndAddJsonAnnotations(RegionRequest region, BufferedImag
     )
     export_to_json.annotations << tmp
 
-
-
     // Create a mask using Java2D functionality
 
     def shape = RoiTools.getShape(roi)
-    def imgMask = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY)
     def g2d = imgMask.createGraphics()
     g2d.setColor(Color.WHITE)
     g2d.scale(1.0/downsample, 1.0/downsample)
@@ -237,10 +242,51 @@ def generateAndSaveMasksAndAddJsonAnnotations(RegionRequest region, BufferedImag
     g2d.fill(shape)
     g2d.dispose()
 
-
-    // Export the mask
-    def fileMask = new File(pathOutput, baseFileName + tmp.name + '_' + '-mask.png')
-    ImageIO.write(imgMask, 'PNG', fileMask)
-
 }
+
+/**
+ * Sort annotations based on annotation name and class
+ *
+ * @param annotations - unsorted Collection of annotations
+ * @return LinkedHashMap with Collections of annotations with same name and class - key: class_name
+ */
+LinkedHashMap sortAnnotations(Collection<PathObject> annotations) {
+    def result = new LinkedHashMap()
+    annotations.each {
+        def className = it.getPathClass() == null ? 'None' : it.getPathClass().toString()
+        def annoName = it.name == null ? 'Annotation' : it.name
+        def key = annoName + '_' + className
+        if (result[key] == null) {
+            result.put(key, new ArrayList<PathObject>())
+        }
+        result[key].add(it)
+    }
+    return result
+}
+
+/**
+ * Process annotations sets generated by sortAnnotations() to generate one binary image per set and extract data
+ * for JSON
+ *
+ * @param key - current set key
+ * @param annotations - current set Collection of annotations
+ * @param region - the global region request
+ * @param img - global image
+ * @param pathOutput - output path for data
+ * @param baseFileName - common filename
+ * @param downsample - global downsampling factor
+ * @param export_to_json - global json object
+ * @return
+ */
+def processAnnotationSet(String key, Collection<PathObject> annotations, RegionRequest region, BufferedImage img,
+                         String pathOutput, String baseFileName, double downsample, imgCellsExport export_to_json) {
+    def annotationMask = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY)
+    annotations.each {
+        generateMaskAndAddJsonAnnotation(region, annotationMask, it, downsample, export_to_json)
+    }
+
+    def annotationMaskFile = new File(pathOutput, baseFileName + key + '_' + '-mask.png')
+    ImageIO.write(annotationMask, 'PNG', annotationMaskFile)
+}
+
 
